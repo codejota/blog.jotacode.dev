a7a40bb5107c04d66a9c483bd962180140cda5e5
Junior Ribeiro <102438579+codejota@users.noreply.github.com>
2023-11-27 21:52:44 -0300

    add new icons

--- src/utils/get-icon-by-name.ts
+++ src/utils/get-icon-by-name.ts
@@ -1,19 +1,35 @@
 import {
   type Icon,
-  IconBook2,
+  IconBrandAndroid,
+  IconBrandBootstrap,
+  IconBrandCss3,
+  IconBrandDocker,
+  IconBrandGit,
+  IconBrandGithub,
+  IconBrandJavascript,
   IconBrandNextjs,
+  IconBrandNodejs,
   IconBrandPlanetscale,
   IconBrandPrisma,
+  IconBrandPython,
+  IconBrandReact,
   IconBrandTailwind,
   IconBrandTypescript,
-  IconChecks,
-  IconDatabase,
-  IconFriends,
+  IconBrandWindows,
+  IconBug,
+  IconChartBar,
+  IconCloud,
+  IconCode,
+  IconHtml,
   IconLink,
+  IconLock,
   IconMarkdown,
   IconPencil,
   IconPhoto,
-  IconTools
+  IconSearch,
+  IconServer,
+  IconSettings,
+  IconUser
 } from '@tabler/icons-react'
 
 /**
@@ -41,15 +57,6 @@ const getIconByName = (name: string): Icon => {
     case 'Tailwindcss': {
       return IconBrandTailwind
     }
-    case 'Tools': {
-      return IconTools
-    }
-    case 'Checks': {
-      return IconChecks
-    }
-    case 'Book2': {
-      return IconBook2
-    }
     case 'Link': {
       return IconLink
     }
@@ -59,11 +66,69 @@ const getIconByName = (name: string): Icon => {
     case 'Photo': {
       return IconPhoto
     }
-    case 'Friends': {
-      return IconFriends
+    case 'HTML5': {
+      return IconHtml
     }
-    case 'Neon': {
-      return IconDatabase
+    case 'CSS3': {
+      return IconBrandCss3
+    }
+    case 'Bootstrap': {
+      return IconBrandBootstrap
+    }
+    case 'Javascript': {
+      return IconBrandJavascript
+    }
+    case 'React': {
+      return IconBrandReact
+    }
+    case 'GitHub': {
+      return IconBrandGithub
+    }
+    case 'Git': {
+      return IconBrandGit
+    }
+    case 'Server': {
+      return IconServer
+    }
+    case 'Bug': {
+      return IconBug
+    }
+    case 'Code': {
+      return IconCode
+    }
+    case 'Docker': {
+      return IconBrandDocker
+    }
+    case 'Nodejs': {
+      return IconBrandNodejs
+    }
+    case 'Python': {
+      return IconBrandPython
+    }
+
+    case 'Windows': {
+      return IconBrandWindows
+    }
+    case 'Android': {
+      return IconBrandAndroid
+    }
+    case 'ChartBar': {
+      return IconChartBar
+    }
+    case 'Cloud': {
+      return IconCloud
+    }
+    case 'Lock': {
+      return IconLock
+    }
+    case 'Search': {
+      return IconSearch
+    }
+    case 'Settings': {
+      return IconSettings
+    }
+    case 'User': {
+      return IconUser
     }
     default: {
       throw new Error('Icon not found')
